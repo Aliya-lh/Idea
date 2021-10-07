d0f0c34b39df277579f25dad95dafb8e8ada36d8
LiuHuan <1576695530@qq.com>
2021-10-07 22:29:54 +0800

    牛客网练习

---
+++ src/Test/Test.java
@@ -0,0 +1,100 @@
+package Test;
+import java.util.*;
+public class Test {
+    public static void main5(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int num = sc.nextInt();
+            if(num%5==0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+
+
+    public static void main1(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.println((a+b+-c)*d);
+    }
+
+
+
+    public static void main3(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x<0){
+            System.out.print("1");
+        }else if(x==0){
+            System.out.print("0");
+        }else{
+            System.out.print("-1");
+        }
+    }
+
+
+
+
+
+    public static void main4(String[] args){
+        int tmp = 0;
+        int count = 0;
+        for(int i = 0;i<2020;i++){
+            tmp = i;
+            while(tmp>0){
+                if(tmp%10==9){
+                    count++;
+                    break;
+                }
+                tmp=tmp/10;
+            }
+        }
+        System.out.println(count);
+    }
+
+
+    public static void main6(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                a++;
+            }else{
+                b++;
+            }
+        }
+        System.out.println(a+" "+b);
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        float s = 0;
+        float [] a = new float[n];
+        for(int i=0;i<n;i++){
+            a[i] = in.nextFloat();
+            s+=a[i];
+        }
+        s = s/n;
+        Arrays.sort(a);
+        System.out.printf("%.2f",a[n-1]);
+        System.out.printf(" ");
+        System.out.printf("%.2f",a[0]);
+        System.out.printf(" ");
+        System.out.printf("%.2f",s);
+    }
+
+
+}
